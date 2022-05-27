0c3b566bd46e07d9e63dd8437c4e126f7000bafe
allen <allenfaughn@gmail.com>
2022-05-27 12:23:12 -0500

    fixed issue with backend

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -3,6 +3,8 @@ const asyncHandler = require('express-async-handler');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -7,6 +7,27 @@ const { User } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
+
+const validateSignup = [
+  check('email')
+  .exists({ checkFalsy: true })
+  .isEmail()
+  .withMessage('Please provide a valid email.'),
+  check('username')
+  .exists({ checkFalsy: true })
+  .isLength({ min: 4 })
+  .withMessage('Please provide a username with at least 4 characters.'),
+  check('username')
+  .not()
+  .isEmail()
+  .withMessage('Username cannot be an email.'),
+  check('password')
+  .exists({ checkFalsy: true })
+  .isLength({ min: 6 })
+  .withMessage('Password must be 6 characters or more.'),
+  handleValidationErrors
+];
+
 router.post(
     '/',
     validateSignup,
@@ -21,25 +42,5 @@ router.post(
       });
     }),
   );
-
-  const validateSignup = [
-    check('email')
-      .exists({ checkFalsy: true })
-      .isEmail()
-      .withMessage('Please provide a valid email.'),
-    check('username')
-      .exists({ checkFalsy: true })
-      .isLength({ min: 4 })
-      .withMessage('Please provide a username with at least 4 characters.'),
-    check('username')
-      .not()
-      .isEmail()
-      .withMessage('Username cannot be an email.'),
-    check('password')
-      .exists({ checkFalsy: true })
-      .isLength({ min: 6 })
-      .withMessage('Password must be 6 characters or more.'),
-    handleValidationErrors
-  ];
 
 module.exports = router;
