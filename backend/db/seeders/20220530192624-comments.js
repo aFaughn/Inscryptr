9d93e19097d9d1a8f9b55a97fbf4e33f003e4093
allen <allenfaughn@gmail.com>
2022-09-07 13:47:06 -0500

    added more seeders

--- backend/db/seeders/20220530192624-comments.js
+++ backend/db/seeders/20220530192624-comments.js
@@ -4,7 +4,8 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
       //userId, cardId, comment
       return queryInterface.bulkInsert('Comments', [
-        {userId: 1, cardId: 1, comment: 'This is a seed comment.', createdAt: new Date(), updatedAt: new Date()}
+        {userId: 1, cardId: 1, comment: 'Meh, I like this card, but it could be better.', createdAt: new Date(), updatedAt: new Date()},
+        {userId: 2, cardId: 1, comment: 'This is my favorite card! I do jumping jacks every time I pull it!', createdAt: new Date(), updatedAt: new Date()}
     ], {});
   },
 
