5a81df89262f2dc8066d9b08806439a7e695c10a
aFaughn <allenfaughn@gmail.com>
2024-02-21 02:09:30 -0600

    added more flips and flaps for variety

--- frontend/src/components/Cards/index.js
+++ frontend/src/components/Cards/index.js
@@ -10,9 +10,11 @@ const Cards = () => {
     const dispatch = useDispatch();
     const cards = useSelector(state=>state.cards.cards);
 
-    const flip1 = new Audio('/sound/flip1.wav')
-    const flip2 = new Audio('/sound/flip2.wav')
-    const flip3 = new Audio('/sound/flip3.wav')
+    const flip1 = new Audio('/sound/cardflips/flip1.wav')
+    const flip2 = new Audio('/sound/cardflips/flip2.wav')
+    const flip3 = new Audio('/sound/cardflips/flip3.wav')
+    const flip4 = new Audio('/sound/cardflips/flip4.wav')
+    const flip5 = new Audio('/sound/cardflips/flip5.wav')
     const flips = new Array()
     flips.push(flip1, flip2, flip3)
     const GetRandomFlip = (max) => {
@@ -31,8 +33,8 @@ const Cards = () => {
                 <div className='cards-wrapper'>
                 <Link className='create-new-card' to='/cards/new'><img alt='addNewCard' src='https://i.imgur.com/hpG52bc.png'></img></Link>
                 {cards.map((card) => (
-                    <div className='cardContainer'>
-                        <div className='card' key={card.id} onMouseEnter={() => flips[GetRandomFlip(flips.length)].play()}>
+                    <div className='cardContainer'  onMouseEnter={() => flips[GetRandomFlip(flips.length)].play()}>
+                        <div className='card' key={card.id}>
                             <Link to={`/cards/${card.id}`}><img alt={card.name} className='cardArt' onError={(e) => e.target.src = 'https://i.imgur.com/lJrAYOk.png'} src={card.imageUrl}></img></Link>
                         </div>
                     </div>
